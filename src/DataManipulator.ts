8e39ac6ad7d2565541fdff1249cb4c435d42c972
taivu1998 <46636857+taivu1998@users.noreply.github.com>
2020-09-08 20:19:50 -0700

    Fix spacing

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -10,26 +10,23 @@ export interface Row {
   trigger_alert: number | undefined,
 }
 
-
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]): Row {
-  	// const priceABC = (serverResponds[0].top_ask_price + serverResponds[0].top_bid_price) / 2;
-		// const priceDEF = (serverResponds[1].top_ask_price + serverResponds[1].top_bid_price) / 2;
-		const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
-  	const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
-  	const ratio = priceABC / priceDEF;
-  	const upperBound = 1 + 0.05;
-  	const lowerBound = 1 - 0.05;
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
 
-  	return {
-  	  price_abc: priceABC,
-  	  price_def: priceDEF,
-  	  ratio: ratio,
-  	  timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ?
-  	    serverResponds[0].timestamp : serverResponds[1].timestamp,
-  	  upper_bound: upperBound,
-  	  lower_bound: lowerBound,
-  	  trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
-  	};
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio: ratio,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ?
+        serverResponds[0].timestamp : serverResponds[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+    };
   }
 }
